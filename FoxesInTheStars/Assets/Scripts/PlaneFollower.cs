c7a76818e6a0179910c5a949bc1e710f18110720
Isaac Argueta <blessthefallensouls@gmail.com>
2018-05-26 16:53:44 -0700

    Adding project files

---
+++ FoxesInTheStars/Assets/Scripts/PlaneFollower.cs
@@ -0,0 +1,23 @@
+﻿ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneFollower : MonoBehaviour
+{
+    public Camera followingCamera;
+    private Transform transform;
+    private Vector3 initialCameraPos;
+	void Start ()
+	{
+	    transform = GetComponent<Transform>();
+	    followingCamera.transform.position = new Vector3(transform.position.x, transform.position.y, followingCamera.transform.position.z);
+	    initialCameraPos = followingCamera.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    Vector3 newCameraPos = Vector3.Lerp(followingCamera.transform.position, new Vector3(transform.position.x, transform.position.y), .2f);
+        followingCamera.transform.position = new Vector3(newCameraPos.x, newCameraPos.y, followingCamera.transform.position.z);
+	}
+}
---
+++ FoxesInTheStars/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Transform transform;
+    [Range(0.1f, 1f)]
+    public float Speed;
+	void Start ()
+	{
+	    transform = GetComponent<Transform>();
+	}
+	
+	
+	void Update ()
+	{
+	    if (Input.GetKey(KeyCode.D))
+	    {
+	        transform.position = new Vector3(transform.position.x + Speed, transform.position.y);
+	    }
+
+	    if (Input.GetKey(KeyCode.A))
+	    {
+	        transform.position = new Vector3(transform.position.x - Speed, transform.position.y);
+        }
+	}
+}
